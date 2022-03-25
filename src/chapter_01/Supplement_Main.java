01eafad99ddfac1a9cedfc4533edc99132486d7c
user <user@DESKTOP-L2KK7E8>
2022-03-25 15:13:10 +0900

    2020.03.25

---
+++ src/chapter_01/Supplement_Main.java
@@ -0,0 +1,19 @@
+package chapter_01;
+
+import java.util.Scanner;
+
+public class Supplement_Main {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+	}
+	
+	
+
+}
