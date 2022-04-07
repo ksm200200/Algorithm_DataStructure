aeb5f3510903badf01f7fb753d2c6c719f1c45c4
user <user@DESKTOP-L2KK7E8>
2022-04-08 00:10:20 +0900

    2022.04.07

--- src/chapter_03/Exercise.java
+++ src/chapter_03/Exercise.java
@@ -1,9 +1,62 @@
 package chapter_03;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Exercise {
 	
+	//실습 3-8
+	static class PhyscData {
+		private String name;     //이름
+		private int height;      //키
+		private double vision;   //시력
+		
+		public PhyscData(String name, int height, double vision) {
+			
+			this.name = name;
+			this.height = height;
+			this.vision = vision;
+		}
+		
+		public String toString() {
+			return name + " " + height + " " + vision;
+		}
+		
+		public static final Comparator<PhyscData> HEIGHT_ORDER = new HeightOrderComparaotor();
+
+		private static class HeightOrderComparaotor implements Comparator<PhyscData>{
+			public int compare(PhyscData d1, PhyscData d2) {
+				return (d1.height > d2.height) ? 1 :
+					(d1.height < d2.height) ? -1 : 0;
+			}
+		}
+	}
+	
+	
+	
+	//실습 3-4
+	static int binSearch(int[] a, int n, int key) {
+		int pl = 0;   //첫번째 요소
+		int pr = n-1; // 마지막 요소
+		
+		do {
+			int pc = (pl+pr) / 2;      //중앙 요소의 인덱스
+			
+			if(a[pc] == key) {         //검색 성공!
+				return pc;            
+			} else if(a[pc] <  key) {  // 검색 범위를 뒤쪽 절반으로 좁힘
+				pl = pc + 1;          
+			} else {                   // 검색 범위를 앞쪽 절반으로 좁힘
+				pr = pc - 1;
+			} 
+			
+		} while(pl <= pr);
+		return -1;
+	}
+	
+	
+
 	//실습 3-3
 	static int seqSearchSen(int a[], int n, int key) {
 		int i = 0;
@@ -44,7 +97,120 @@ public class Exercise {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		//실습 3-3
+
+		/* 실습 3-8
+		PhyscData[] x = {
+				new PhyscData("이나령", 162, 0.3),
+				new PhyscData("유지훈", 168, 0.4),
+				new PhyscData("김한결", 169, 0.8),
+				new PhyscData("홍준기", 171, 1.5),
+				new PhyscData("전서현", 173, 0.7),
+				new PhyscData("이호연", 174, 1.2),
+				new PhyscData("이수민", 175, 2.0),
+		};
+		
+		System.out.print("몇 cm인 사람을 찾고 있나요? >> ");
+		int height = sc.nextInt();
+		int idx = Arrays.binarySearch(x, new PhyscData("", height, 0.0), PhyscData.HEIGHT_ORDER);
+		
+		if(idx < 0) {
+			System.out.println("요소가 없습니다.");
+		} else {
+			System.out.println("x[" + idx + "]에 있습니다.");
+			System.out.println("찾은 데이터: " + x[idx]);
+		}
+		*/
+		
+		/*실습 3-6
+		String[] x = {
+				"abstract",   "assert",       "boolean",   "break",      "byte",
+				"case",       "catch",        "char",      "class",      "const",
+				"continue",   "default",      "do",        "double",     "else",
+				"enum",       "extends",      "final",     "finally",    "float",
+				"for",        "goto",         "if",        "implements", "import",
+				"instanceof", "int",          "interface", "long",       "native",
+				"new",        "package",      "private",   "protected",  "public",
+				"return",     "short",        "static",    "strictfp",   "super",
+				"switch",     "synchronized", "this",      "throw",      "throws",
+				"transient",  "try",          "void",      "volatile",   "while"
+			};
+		
+		System.out.print("원하는 키워드를 입력하세요. >> ");
+		String ky = sc.next();
+		int idx = Arrays.binarySearch(x, ky);
+		
+		if(idx < 0) {
+			System.out.println("해당 키워드가 없습니다.");
+		} else {
+			System.out.println("해당 키워드는  x[" + idx + "]에 있습니다.");
+		}
+		*/
+		
+		
+		/*실습 3-5
+		System.out.print("요솟수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		
+		System.out.println("오름차순으로 입력하세요.");
+		
+		System.out.print("x[0]: ");
+		x[0] = sc.nextInt();
+		
+		for(int i = 1; i < num; i++) {
+			do {
+				System.out.print("x[" + i + "]: ");
+				x[i] = sc.nextInt();				
+			} while(x[i] < x[i-1]);
+		} 
+		
+		System.out.println("검색할 값: ");
+		int ky = sc.nextInt();
+		//기본 자료형 메소드
+		int idx = Arrays.binarySearch(x, ky);    //배열 x에서 키 값이 ky인 요소를 검색
+		
+		if(idx < 0) {
+			System.out.println("그 값의 요소가 없습니다.");
+		} else {
+			System.out.println(ky +"은(는) x[" + idx + "]에 있습니다.");
+		}
+		*/
+		
+		
+		
+		/*실습 3-4
+		System.out.print("요솟수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		
+		System.out.println("오름차순으로 입력하세요.");
+		System.out.print("x[0]: " ); //첫 요소 입력
+		x[0] = sc.nextInt();
+		
+		for(int i = 1; i < num; i++) {
+			do {
+				System.out.print("x[" + i + "]: ");
+				x[i] = sc.nextInt();
+			} while(x[i] < x[i-1]); //값이 바로 앞의 요소보다 작으면 다시 입력
+			
+		} 
+		
+		System.out.print("검색할 값: ");
+		int ky = sc.nextInt();
+		int idx = binSearch(x, num, ky);
+		
+		if(idx == -1) {
+			System.out.println("그 값의 요소가 없습니다.");
+		} else {
+			System.out.println(ky + "은(는)" + "x[" + idx + "]에 있습니다.");
+			
+		}
+		*/
+		
+		
+		
+		
+		/*실습 3-3
 		System.out.print("요솟수: ");
 		int num = sc.nextInt();
 		int[] x = new int[num + 1]; //요솟수 num +1
@@ -64,7 +230,7 @@ public class Exercise {
 		} else {
 			System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
 		}
-		
+		*/
 		
 		/*실습 3-1
 		System.out.print("요솟수: ");
--- src/chapter_03/Practice.java
+++ src/chapter_03/Practice.java
@@ -1,5 +1,228 @@
 package chapter_03;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
 public class Practice {
+	
+	//연습문제 3-7
+	static class PhyscData {
+		private String name;     //이름
+		private int height;      //키
+		private double vision;   //시력
+		
+		public PhyscData(String name, int height, double vision) {
+			
+			this.name = name;
+			this.height = height;
+			this.vision = vision;
+		}
+		
+		public String toString() {
+			return name + " " + height + " " + vision;
+		}
+		
+		public static final Comparator<PhyscData> VISION_ORDER = new VisionOrderComparaotor();
+
+		private static class VisionOrderComparaotor implements Comparator<PhyscData>{
+			public int compare(PhyscData d1, PhyscData d2) {
+				return (d1.vision > d2.vision) ? 1 :
+					(d1.vision < d2.vision) ? -1 : 0;
+			}
+		}
+	}
+	
+	
+	//연습문제 3-3
+	static int searchIdx(int[] a, int n, int key, int[] idx) {
+		int count = 0;
+		for(int i = 0; i < n; i++) {
+			if(a[i] == key) {
+				idx[count] = i;
+				count++;
+			}
+		}
+		return count;
+	}
+
+	
+	
+	//연습문제 3-2
+	static int seqSearchEx(int[] a, int n, int key) {
+		System.out.print("   |");
+		for (int k = 0; k < n; k++)
+			System.out.printf("%4d", k);
+		System.out.println();
+
+		System.out.print("---+");
+		for (int k = 0; k < 4 * n + 2; k++)
+			System.out.print("-");
+		System.out.println();
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("   |");
+			System.out.printf(String.format("%%%ds*\n", (i * 4) + 3), "");
+			System.out.printf("%3d|", i);
+			for (int k = 0; k < n; k++)
+				System.out.printf("%4d", a[k]);
+			System.out.println("\n   |");
+			if (a[i] == key)
+				return i; // 검색성공
+		}
+		return -1; // 검색실패
+	}
+		
+		
+		
+	//연습문제 3-1	
+	static int seqSearchSen(int a[], int n, int key) {
+		int i;
+		a[n] = key;     //보초를 추가
+			
+		for(i = 0; i < n; i++ ) {
+			if(a[i] == key) {
+				break;     //검색 성공!
+			}
+			
+		}
+		return i == n ? -1 : i;
+	}
+	
+	public static void main(String agrs[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		/*연습문제 3-7
+		PhyscData[] x = {
+				new PhyscData("이나령", 162, 0.3),
+				new PhyscData("유지훈", 168, 0.4),
+				new PhyscData("김한결", 169, 0.8),
+				new PhyscData("홍준기", 171, 1.5),
+				new PhyscData("전서현", 173, 0.7),
+				new PhyscData("이호연", 174, 1.2),
+				new PhyscData("이수민", 175, 2.0),
+		};
+		System.out.print("찾으시는 시력이 몇입니까? >> ");
+		double vision = sc.nextDouble();
+		int idx = Arrays.binarySearch(x, new PhyscData("", 0, vision), PhyscData.VISION_ORDER);
+		
+		if(idx < 0) {
+			System.out.println("요소가 없습니다.");
+		} else {
+			System.out.println("x[" + idx + "]에 있습니다.");
+			System.out.println("찾은 데이터: " + x[idx]);
+		}
+		*/
+		
+		
+		
+		/*연습문제 3-6
+		System.out.print("요솟수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		
+		System.out.println("오름차순으로 입력하세요.");
+		
+		System.out.print("x[0]: ");
+		x[0] = sc.nextInt();
+		
+		for(int i = 1; i < num; i++) {
+			do {
+				System.out.print("x[" + i + "]: ");
+				x[i] = sc.nextInt();				
+			} while(x[i] < x[i-1]);
+		} 
+		
+		System.out.println("검색할 값: ");
+		int ky = sc.nextInt();
+		//기본 자료형 메소드
+		int idx = Arrays.binarySearch(x, ky);    //배열 x에서 키 값이 ky인 요소를 검색
+		
+		if(idx < 0) {
+			int insPoint = -idx - 1;
+			System.out.println("그 값의 요소가 없습니다.");
+			System.out.printf("삽입 포인트는 %d입니다.\n", insPoint);
+			System.out.printf("x[%d]의 바로 앞에 %d를 삽입하면 배열의 정렬 상태가 유지됩니다.", insPoint, ky);
+		} else {
+			System.out.println(ky +"은(는) x[" + idx + "]에 있습니다.");
+		}
+		*/
+		
+		
+		/*연습문제 3-3
+		System.out.print("요솟수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		int[] y = new int[num];
+		
+		for(int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "]: ");
+			x[i] = sc.nextInt();
+		}
+		
+		System.out.print("검색할 값: ");
+		int ky = sc.nextInt();
+		
+		int count = searchIdx(x, num, ky, y);
+		
+		if(count == 0) {
+			System.out.println("그 값의 요소가 없습니다.");
+		} else {
+			System.out.println("key의 개수는: " + count + "개이고" );
+			for(int i = 0; i < count; i++) {
+				System.out.println("그 값은 " + "x[" +y[i] + "]에 있습니다.");
+			}
+			
+		}
+		*/		
+		
+		/*연습문제 3-2
+		System.out.print("요솟수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num + 1]; //요솟수 num +1
+		
+		for(int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "]: ");
+			x[i] = sc.nextInt();
+		}
+		
+		System.out.println("검색할 값: ");
+		int ky = sc.nextInt();
+		
+		int idx = seqSearchEx(x, num, ky);
+		
+		if(idx == -1) {
+			System.out.println("그 값의 요소가 없습니다.");
+		} else {
+			System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+		}
+		*/
+		
+		
+		/*연습문제 3-1
+		System.out.print("요솟수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num + 1]; //요솟수 num +1
+		
+		for(int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "]: ");
+			x[i] = sc.nextInt();
+		}
+		
+		System.out.println("검색할 값: ");
+		int ky = sc.nextInt();
+		
+		int idx = seqSearchSen(x, num, ky);
+		
+		if(idx == -1) {
+			System.out.println("그 값의 요소가 없습니다.");
+		} else {
+			System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+		}
+		*/
+		
+	}
 
 }
